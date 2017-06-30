b6cb0f97a7c68cad1e2f72687ae76b09bbcd2b4c
Greg Bateman <gerbot150@gmail.com>
2017-06-29 20:00:59 -0400

    Added friend summoner IDs and a simple test

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
+
+const leagueEndpoint = "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/$";
+
+const riotHeader = {
+  "X-Riot-Token": process.env.RIOT_API_KEY
+}
+
+const myId = 30409517;
+const friendIds = [
+  43622871, // John
+  42722081, // Andrew
+  30370679, // Noah
+  19111203, // Connor
+  31856445, // Stevie
+  36570422, // Cam
+  37710415, // Gragon
+  35849909, // Javidd
+  35738225, // Gerrard
+]
 
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
+  console.log(fetch(leagueEndpoint.replace('$', myId)));
   response.send('<p>test</p>');
 });
 
