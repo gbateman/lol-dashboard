85dc452ab056f19212934645a1f43895ffbf0560
Greg Bateman <gerbot150@gmail.com>
2017-06-30 15:20:06 -0400

    Refactored

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const request = require('request');
 const leagueEndpoint = "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/$";
 
 const riotHeader = {
-  "Origin": "https://developer.riotgames.com",
-  "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
   "X-Riot-Token": process.env.RIOT_API_KEY
 }
 
@@ -78,53 +76,53 @@ function soloQueueRankFromJSON(json) {
     "effectiveRank": function() {
       return tiers.indexOf(this.tier) * 505
                     + ranks.indexOf(this.rank) * 101
-                    + this.leaguePoints
+                    + this.leaguePoints;
     }
   } : null;
 }
 
-app.get('/', function(req, res) {
-  request(optionsWithId(myId), function(error, response, body) {
+function requestWithId(id, res, callback) {
+  request(optionsWithId(id), function(error, response, body) {
     if (!error && response.statusCode == 200) {
       const json = JSON.parse(body);
-      myRank = soloQueueRankFromJSON(json);
-
-      let responseCounter = 0;
-      for(let i = 0; i < friendIds.length; i++) {
-        if(!friendRanks[friendIds[i]]) {
-          request(optionsWithId(friendIds[i]), function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              const json = JSON.parse(body);
-              let friendRank = soloQueueRankFromJSON(json);
-              if(friendRank) {
-                friendRanks[friendIds[i]] = friendRank;
-              }
-
-              responseCounter++;
-              if(responseCounter == friendIds.length) {
-                let allRanks = Object.assign({myId: myRank}, friendRanks);
-                const sortedKeys = Object.keys(allRanks).sort(function(a, b){
-                  return allRanks[b].effectiveRank() - allRanks[a].effectiveRank();
-                });
-                allRanks = sortedKeys.map(function(key) {
-                  return allRanks[key];
-                });
-
-                res.render('index', {ranks: allRanks});
-              }
-            } else {
-              res.status(400);
-              res.send(error);
-              return;
-            }
-          });
-        } else {
-          responseCounter++;
-        }
-      }
+      callback(json);
     } else {
+      error && console.log(error);
+      console.log(response.statusCode);
       res.status(400);
       res.send(error);
+    }
+  });
+}
+
+app.get('/', function(req, res) {
+  requestWithId(myId, res, function(json){
+    myRank = soloQueueRankFromJSON(json);
+
+    let requestCounter = 0;
+    for(let i = 0; i < friendIds.length; i++) {
+      if(!friendRanks[friendIds[i]]) {
+        requestCounter++;
+        requestWithId(friendIds[i], res, function(json){
+          let friendRank = soloQueueRankFromJSON(json);
+          if(friendRank) {
+            friendRanks[friendIds[i]] = friendRank;
+          }
+
+          requestCounter--;
+          if(requestCounter == 0) {
+            let allRanks = Object.assign({myId: myRank}, friendRanks);
+            const sortedIds = Object.keys(allRanks).sort(function(a, b){
+              return allRanks[b].effectiveRank() - allRanks[a].effectiveRank();
+            });
+            allRanks = sortedIds.map(function(key) {
+              return allRanks[key];
+            });
+
+            res.render('index', {ranks: allRanks});
+          }
+        });
+      }
     }
   });
 });
