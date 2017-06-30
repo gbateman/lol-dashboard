24bc880279ffdffecfa96eb389c46f6ad659eddf
Greg Bateman <gerbot150@gmail.com>
2017-06-30 02:26:10 -0400

    Gets summoner solo queue ranks and prints them

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-const fetch = require('node-fetch');
+const request = require('request');
 
 const leagueEndpoint = "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/$";
 
 const riotHeader = {
+  "Origin": "https://developer.riotgames.com",
+  "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
   "X-Riot-Token": process.env.RIOT_API_KEY
 }
 
@@ -22,17 +24,76 @@ const friendIds = [
   35738225, // Gerrard
 ]
 
+let myRank;
+let friendRanks = {};
+
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  fetch(leagueEndpoint.replace('$', myId), {header: riotHeader}).then(function(res) {
-    return res.json();
-  }).then(function(json) {
-    console.log(json);
-  })
-  response.send('<p>test</p>');
+function optionsWithId(id) {
+  return {
+    url: leagueEndpoint.replace('$', id),
+    headers: riotHeader
+  }
+}
+
+function soloQueueRankFromJSON(json) {
+  let soloQueueRank;
+  for(let i = 0; i < json.length; i++) {
+    if(json[0].queueType == 'RANKED_SOLO_5x5') {
+      soloQueueRank = json[i];
+      break;
+    }
+  }
+  return soloQueueRank ? {
+    "tier": soloQueueRank.tier,
+    "queueType": soloQueueRank.queueType,
+    "rank": soloQueueRank.rank,
+    "name": soloQueueRank.playerOrTeamName,
+    "leaguePoints": soloQueueRank.leaguePoints,
+    "wins": soloQueueRank.wins,
+    "losses": soloQueueRank.losses,
+  } : null;
+}
+
+app.get('/', function(req, res) {
+  request(optionsWithId(myId), function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const json = JSON.parse(body);
+      myRank = soloQueueRankFromJSON(json);
+
+      let responseCounter = 0;
+      for(let i = 0; i < friendIds.length; i++) {
+        if(!friendRanks[friendIds[i]]) {
+          request(optionsWithId(friendIds[i]), function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+              console.log(response);
+              const json = JSON.parse(body);
+              let friendRank = soloQueueRankFromJSON(json);
+              if(friendRank) {
+                friendRanks[friendIds[i]] = friendRank;
+              }
+
+              responseCounter++;
+              if(responseCounter == friendIds.length) {
+                res.send(friendRanks);
+              }
+            } else {
+              res.status(400);
+              res.send(error);
+              return;
+            }
+          });
+        } else {
+          responseCounter++;
+        }
+      }
+    } else {
+      res.status(400);
+      res.send(error);
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
